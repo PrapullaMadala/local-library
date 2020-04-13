4852965e8b4c9fddd213e738afc4f0c4094fc164
prapula <prapula.m@gmail.com>
2020-04-13 18:44:55 -0400

    modified

--- library/catalogapp/models.py
+++ library/catalogapp/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 
 class Genre(models.Model):
-    """Model representing a book genre."""
+
     book_kind = models.CharField(max_length=200,
                                  help_text='Enter a book genre (e.g. Science Fiction)')
 
