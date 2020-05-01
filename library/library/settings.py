120fd57b785323c7c936690c50dc2cf6350962ce
prapula <prapula.m@gmail.com>
2020-05-01 13:58:37 -0400

    static root modified in settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#e3ciu90+2jv@prxcnic+wbh(47&!dfzyb8!md@xc&-g)%q_dc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
