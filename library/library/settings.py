eefd51bcf10bb8cd3c06eafd0c2ba75324293f91
prapula <prapula.m@gmail.com>
2020-05-04 14:28:44 -0400

    'modified'

--- library/library/settings.py
+++ library/library/settings.py
@@ -85,16 +85,28 @@ WSGI_APPLICATION = 'library.wsgi.application'
     }
 }'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'library',
-        'USER': 'psql',
-        'PASSWORD': 'ajayprapu15',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'library',
+            'USER': 'psql',
+            'PASSWORD': 'ajayprapu15',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
