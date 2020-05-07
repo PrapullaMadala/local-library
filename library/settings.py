ec9561fd8b2c3d76e4bb0f2b03792e668577562a
prapula <prapula.m@gmail.com>
2020-05-07 10:21:55 -0400

    'modified'

--- library/settings.py
+++ library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#e3ciu90+2jv@prxcnic+wbh(47&!d
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1', 'library-dev.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['172.31.47.247', '.elasticbeanstalk.com', '127.0.0.1', 'library-dev.us-east-1.elasticbeanstalk.com']
 
 # Application definition
 
